0e6fe1c814b2e20e46c26914d617c718a4b3f97d
Cristianmenegassobianco <cristianmenegassobianco@gmail.com>
2026-07-07 21:04:51 -0300

    fix: Garantir carregamento do .env via package.json

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,4 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, '.env') });
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
